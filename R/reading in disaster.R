3439ad437245f02dd4ccb02ac8b2f0e60c3c4d22
Kayla Esser <kaylaesser@gmail.com>
2023-09-18 12:57:43 -0400

    Adding disaster data and cleaned disaster data

---
+++ R/reading in disaster.R
@@ -0,0 +1,19 @@
+#reading in and cleaning disaster data
+library(here)
+library(magrittr)
+library(dplyr)
+here()
+rawdis <- read.csv(here("original", "disaster.csv"), header = TRUE)
+rawdis
+cleandis <- rawdis %>% 
+  filter(between(Year,2000,2019)) %>%
+  filter(Disaster.Type %in% c("Drought", "Earthquake")) %>%
+  select(Year, ISO, Disaster.Type)
+
+cleandis$drought <- ifelse(cleandis$Disaster.Type == "Drought", 1, 0)
+cleandis$earthquake <- ifelse(cleandis$Disaster.Type == "Earthquake", 1, 0)
+
+cleandis <- cleandis %>%
+  group_by(Year, ISO) %>%
+  summarize(drought = sum(drought), earthquake = sum(earthquake))
+
